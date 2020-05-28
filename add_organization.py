238e807fc14826cc3b275bb6fe0e99e799a094a6
Michael <turtlebasket@gmail.com>
2020-05-27 21:47:31 -0700

    more organization-adding stuff. de-jankify later.

--- add_organization.py
+++ add_organization.py
@@ -6,6 +6,11 @@ from json import load as json_load, dumps as json_dumps
 I'd like to get rid of this file.
 We shouldn't have to do this.
 - Trinity
+
+I know, it's definitely not ideal. :P
+We'll eventually replace the crappy CLI version with a proper web form 
+once we start taking customers (That is, unless we find an even better way).
+- Michael
 """
 
 credman = load_credentialsmanager()
@@ -20,11 +25,15 @@ while True:
         break
     else:
         print("Passwords did not match. Please try again.")
+    
+domains_str = input("Enter all user domains, separated by commas: ")
+domains = [i.strip() for i in domains_str.split(",")]
 
 orgs.append({
     "user": username,
     "sh_pw": credman.sh_pw(password), 
-    "id": gen_org_id([i["id"] for i in orgs])
+    "id": gen_org_id([i["id"] for i in orgs]),
+    "domains": domains
 })
 
 print(f"Adding {username}.")
