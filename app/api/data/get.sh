1e77944f35dc3b78f00735ef32472617a74432d7
turtlebasket <turtlebasket@gmail.com>
2020-06-14 22:43:15 -0700

    linux TLC

--- app/api/data/get.sh
+++ app/api/data/get.sh
@@ -1,4 +1,4 @@
-curl http://data.phishtank.com/data/online-valid.csv -o verified_online.csv
-curl https://www.domcop.com/files/top/top10milliondomains.csv.zip -o top10milliondomains.csv.zip
+curl -L http://data.phishtank.com/data/online-valid.csv -o verified_online.csv
+curl -L https://www.domcop.com/files/top/top10milliondomains.csv.zip -o top10milliondomains.csv.zip
 unzip top10milliondomains.csv.zip 
 rm top10milliondomains.csv.zip
