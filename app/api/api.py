0e32efc722e5377ef3d37dbcb9291b3fd6d36103
turtlebasket <turtlebasket@gmail.com>
2020-10-06 20:19:11 -0700

    preemptively check client key

--- app/api/api.py
+++ app/api/api.py
@@ -19,70 +19,98 @@ cors = CORS(api_bp, resources={r"/api/*": {"origins": "*"}})
 
 @api_bp.route("/api/rate-risk/link", methods=['GET'])
 def rate_risk_link():
-    url = request.args.get('url')
+
     api_key = request.args.get('key')
-    return (rate_link(url)) if sha256(api_key.encode()).hexdigest() == get_ext_key() else 400
+    if sha256(api_key.encode()).hexdigest() != get_ext_key():
+        return 400
+
+    url = request.args.get('url')
+    return (rate_link(url))
 
 
 
 
 @api_bp.route("/api/report", methods=['POST', 'PUT'])
 def handle_report():
-    data = request.get_json()
+
     api_key = data['key']
+    if sha256(api_key.encode()).hexdigest() != get_ext_key():
+        return 400
+
+    data = request.get_json()
     report_data = data['report_data']
     org_id = request.get_json()['org_id']
     report_data['timestamp'] = now()
     rdb().get_table(org_id).insert(report_data)
-    return data, 200 if sha256(api_key.encode()).hexdigest() == get_ext_key() else 400
+    return data, 200
 
 
 @api_bp.route("/api/feedback", methods=['POST', 'PUT']) # keep as is
 def handle_feedback():
-    data = request.get_json()['data']
+    
     api_key = request.get_json()['key']
+    if sha256(api_key.encode()).hexdigest() != get_ext_key():
+        return 400
+
+    data = request.get_json()['data']
     data['timestamp'] = now()
     # fdb = db.get_table('feedback')
     # fdb.insert(data)
     try_discord_send(request.get_json()['discord'])
-    return data, 200 if sha256(api_key.encode()).hexdigest() == get_ext_key() else 400
+    return data, 200
 
 
 @api_bp.route("/api/bug", methods=['POST', 'PUT']) # keep as is
 def handle_bug():
-    data = request.get_json()['data']
+
     api_key = request.get_json()['key']
+    if sha256(api_key.encode()).hexdigest() != get_ext_key():
+        return 400
+
+    data = request.get_json()['data']
     data['timestamp'] = now()
     # bdb() = db.get_table('bugs')
     # bdb().insert(data)
     try_discord_send(request.get_json()['discord'])
-    return data, 200 if sha256(api_key.encode()).hexdigest() == get_ext_key() else 400
+    return data, 200
 
 
 @api_bp.route("/api/blacklist", methods=['GET'])
 def get_blacklist():
-    args = request.args
+
     api_key = request.args.get('key')
+    if sha256(api_key.encode()).hexdigest() != get_ext_key():
+        return 400
+
+    args = request.args
     b1 = [item['address'] for item in bdb()[args.get('org_id')].all()]
     return {
         'data': b1,
-    } if sha256(api_key.encode()).hexdigest() == get_ext_key() else 400
+    }
 
 
 @api_bp.route("/api/whitelist", methods=['GET'])
 def get_whitelist():
-    args = request.args
+
     api_key = request.args.get('key')
+    if sha256(api_key.encode()).hexdigest() != get_ext_key():
+        return 400
+
+    args = request.args
     w1 = [item['address'] for item in wdb()[args.get('org_id')].all()]
     return {
         'data': w1,
-    } if sha256(api_key.encode()).hexdigest() == get_ext_key() else 400
+    }
 
 @api_bp.route("/api/verify_email", methods=['GET'])
 def verify_email():
 
-    target = request.args.get('address')
     api_key = request.args.get('key')
+    if sha256(api_key.encode()).hexdigest() != get_ext_key():
+        return 400
+
+
+    target = request.args.get('address')
 
     # unclassified --> 0
     numrating = 0
@@ -97,15 +125,19 @@ def verify_email():
         if re.match(item['address'], target):
             numrating = 2
     
-    return {'status': numrating} if sha256(api_key.encode()).hexdigest() == get_ext_key() else 400
+    return {'status': numrating}
 
 @api_bp.route("/api/get_org", methods=['GET'])
 def get_organization():
+
+    api_key = args.get('key')
+    if sha256(api_key.encode()).hexdigest() != get_ext_key():
+        return 400
+
     # json = request.get_json()
     # target_domain = json['address'].split("@")[1]
     # orgs = load_organizations()
     args = request.args
-    api_key = args.get('key')
     target_domain = args.get('address').split("@")[1]
     orgs = load_organizations()  
 
@@ -119,18 +151,22 @@ def get_organization():
     
     return {
         'ID': ID, 
-    }, 200 if sha256(api_key.encode()).hexdigest() == get_ext_key() else 400
+    }, 200
 
 @api_bp.route("/api/get_support_emails", methods=['GET'])
 def get_support_emails():
     api_key = request.args.get('key')
+    if sha256(api_key.encode()).hexdigest() != get_ext_key():
+        return 400
+
+
     orgs = load_organizations()
 
     for o in orgs:
         if o['id'] == request.args['id']:
             return {
                 'support_emails': o['support_emails']
-            }, 200 if sha256(api_key.encode()).hexdigest() == get_ext_key() else 400
+            }, 200
     return {} 
 
 def rel_path(string):
