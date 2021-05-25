825da3efa3dff315fbeed8f6c837a56c959fbf68
turtlebasket <turtlebasket@gmail.com>
2021-05-25 22:04:48 +0000

    move domain to freenom

--- cert.sh
+++ cert.sh
@@ -2,7 +2,7 @@ echo -n $'[1] Generate Certificate\n[2] Renew Certificate\n>'
 read opt
 
 if [ $opt == 1 ] ; then
-	sudo sudo certbot certonly --standalone --preferred-challenges http -d veritas.computer
+	sudo sudo certbot certonly --standalone --preferred-challenges http -d aspys.tk
 	sudo certbot --nginx
 elif [ $opt == 2 ] ; then
 	sudo certbot renew
